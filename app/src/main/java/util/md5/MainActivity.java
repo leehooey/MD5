5b1d9259cffbad7a4a35ad636757a65f4a9b49e8
Lee <1195394563@qq.com>
2016-08-20 09:26:09 +0800

    Initial commit

---
+++ app/src/main/java/util/md5/MainActivity.java
@@ -0,0 +1,14 @@
+package util.md5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
